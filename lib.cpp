7a1e770f1860abacf8cbd98db0f663dd0e0a8f9e
Lorenzo Falbo 5640560 <116544189+Ryato-03@users.noreply.github.com>
2022-12-03 12:48:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+using namespace std;
 
+bool primo(int numero, int i) {
+  int resto=numero%i;
+  
+  if(numero<=i)return true;
+  if(resto==0&&i>1)return false;
+  if(resto!=0) {
+    i++;
+    primo(numero, i);
+  }
+  else return true; 
+}
